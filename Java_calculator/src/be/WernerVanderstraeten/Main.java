ad789ede6d201a52f3c1d95e368e36acb1173b92
Werner Vanderstraeten <wernervstr@hotmail.com>
2019-09-08 18:04:53 +0200

    Update Main.java

--- Java_calculator/src/be/WernerVanderstraeten/Main.java
+++ Java_calculator/src/be/WernerVanderstraeten/Main.java
@@ -1,3 +1,9 @@
+//*****************************************************************************
+// Name: Main
+// Author: Werner Vanderstraeten
+// Description: A seperate Main class with the main() method.
+//*****************************************************************************
+
 package be.WernerVanderstraeten;
 
 import javax.swing.*;
@@ -6,7 +12,7 @@ public class Main {
 
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-
+        
         calculator.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         calculator.pack();
         calculator.setLocationRelativeTo(null);
